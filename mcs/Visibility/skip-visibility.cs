dbedcc195c9ea49e63631a18dc40959f1630800f
ghorsington <neigh@coder.horse>
2020-01-06 18:36:22 +0200

    Fix assemblies not being visible after corlib trick

--- mcs/Visibility/skip-visibility.cs
+++ mcs/Visibility/skip-visibility.cs
@@ -1,17 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
+using MonoMod.RuntimeDetour;
 
 namespace Mono.CSharp
 {
     internal static class SkipVisibilityExt
     {
-        internal static readonly bool _IsMono = Type.GetType("Mono.Runtime") != null;
-        internal static readonly bool _IsNewMonoSRE = _IsMono && typeof(DynamicMethod).GetField("il_info", BindingFlags.NonPublic | BindingFlags.Instance) != null;
-        internal static readonly bool _IsOldMonoSRE = _IsMono && !_IsNewMonoSRE && typeof(DynamicMethod).GetField("ilgen", BindingFlags.NonPublic | BindingFlags.Instance) != null;
+        static readonly bool _IsMono = Type.GetType("Mono.Runtime") != null;
+        static readonly bool _MonoAssemblyNameHasArch = new AssemblyName("Dummy, ProcessorArchitecture=MSIL").ProcessorArchitecture == ProcessorArchitecture.MSIL;
 
-        internal static FieldInfo dynAssField =
+        static readonly FieldInfo dynAssField =
             typeof(AssemblyBuilder).GetField("dynamic_assembly", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        static List<Assembly> generatedAssemblies = new List<Assembly>();
+
+        static SkipVisibilityExt()
+        {
+            new Hook(typeof(AppDomain).GetMethod(nameof(AppDomain.GetAssemblies)), typeof(SkipVisibilityExt).GetMethod(nameof(GetAssembliesPatch))).Apply();
+        }
+
+        public static Assembly[] GetAssembliesPatch(Func<AppDomain, Assembly[]> orig, AppDomain self)
+        {
+            var result = new List<Assembly>(orig(self));
+            result.AddRange(generatedAssemblies);
+            return result.ToArray();
+        }
         
         public static unsafe void MarkCorlibInternal(this AssemblyBuilder asm)
         {
@@ -23,97 +38,57 @@ namespace Mono.CSharp
             if (asmPtr == UIntPtr.Zero)
                 return;
             
-            var offs = 0;
+            var offs = 
+                // ref_count (4 + padding)
+                IntPtr.Size +
+                // basedir
+                IntPtr.Size +
 
-            if (_IsOldMonoSRE)
-            {
-                offs =
-                    // ref_count (4 + padding)
-                    IntPtr.Size +
-                    // basedir
-                    IntPtr.Size +
+                // aname
+                // name
+                IntPtr.Size +
+                // culture
+                IntPtr.Size +
+                // hash_value
+                IntPtr.Size +
+                // public_key
+                IntPtr.Size +
+                // public_key_token (17 + padding)
+                20 +
+                // hash_alg
+                4 +
+                // hash_len
+                4 +
+                // flags
+                4 +
 
-                    // aname
-                    // name
-                    IntPtr.Size +
-                    // culture
-                    IntPtr.Size +
-                    // hash_value
-                    IntPtr.Size +
-                    // public_key
-                    IntPtr.Size +
-                    // public_key_token (17 + padding)
-                    17 + 3 +
-                    // hash_alg
-                    4 +
-                    // hash_len
-                    4 +
-                    // flags
-                    4 +
+                // major, minor, build, revision[, arch] (10 framework / 20 core + padding)
+                (
+                    !_MonoAssemblyNameHasArch ? (
+                        typeof(object).Assembly.GetName().Name == "System.Private.CoreLib" ? 
+                            16 : 8
+                    ) : (
+                        typeof(object).Assembly.GetName().Name == "System.Private.CoreLib" ? 
+                            (IntPtr.Size == 4 ? 20 : 24) :
+                            (IntPtr.Size == 4 ? 12 : 16)
+                    )
+                ) +
 
-                    // major, minor, build, revision
-                    2 + 2 + 2 + 2 +
+                // image
+                IntPtr.Size +
+                // friend_assembly_names
+                IntPtr.Size +
+                // friend_assembly_names_inited
+                1 +
+                // in_gac
+                1 +
+                // dynamic
+                1; ;
 
-                    // image
-                    IntPtr.Size +
-                    // friend_assembly_names
-                    IntPtr.Size +
-                    // friend_assembly_names_inited
-                    1 +
-                    // in_gac
-                    1 +
-                    // dynamic
-                    1;
-            } else if (_IsNewMonoSRE)
-            {
-                offs =
-                    // ref_count (4 + padding)
-                    IntPtr.Size +
-                    // basedir
-                    IntPtr.Size +
-
-                    // aname
-                    // name
-                    IntPtr.Size +
-                    // culture
-                    IntPtr.Size +
-                    // hash_value
-                    IntPtr.Size +
-                    // public_key
-                    IntPtr.Size +
-                    // public_key_token (17 + padding)
-                    20 +
-                    // hash_alg
-                    4 +
-                    // hash_len
-                    4 +
-                    // flags
-                    4 +
-
-                    // major, minor, build, revision, arch (10 framework / 20 core + padding)
-                    (typeof(object).Assembly.GetName().Name == "System.Private.CoreLib" ? IntPtr.Size == 4
-                            ?
-                            20
-                            : 24 :
-                        IntPtr.Size == 4 ? 12 : 16) +
-
-                    // image
-                    IntPtr.Size +
-                    // friend_assembly_names
-                    IntPtr.Size +
-                    // friend_assembly_names_inited
-                    1 +
-                    // in_gac
-                    1 +
-                    // dynamic
-                    1;
-            }
-
-            if (offs == 0)
-                return;
-            
             var corlibInternalPtr = (byte*) ((long) asmPtr + offs);
             *corlibInternalPtr = 1;
+
+            generatedAssemblies.Add(asm);
         }
     }
 }
